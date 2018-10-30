8a8c9d995b45bd228e7c37cf20dcd8f4923fb333
AlexandrovD <alexandrov.dmytro@gmail.com>
2018-10-30 16:21:49 +0200

    Fix tests

--- src/main/java/com/aleksandrov/bootci/config/SecurityConfig.java
+++ src/main/java/com/aleksandrov/bootci/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.aleksandrov.bootci;
+package com.aleksandrov.bootci.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
--- src/test/java/com/aleksandrov/bootci/BootciApplicationIT.java
+++ src/test/java/com/aleksandrov/bootci/BootciApplicationIT.java
@@ -2,12 +2,15 @@ package com.aleksandrov.bootci;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
 @SpringBootTest
-public class BootciApplicationTests {
+
+public class BootciApplicationIT {
 
 	@Test
 	public void contextLoads1() {
